d085e91ac3a732dbbcb5d9e490c311913b8a6b33
feola <feolan@mail.ru>
2012-07-22 21:25:00 +0400

    my first php

---
+++ myphp/date-time.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+<title> Дата и время </title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+</head>
+
+<body>
+
+<h1> Хотите узнать дату и время? </br>
+Пожалуйста:
+</h1>
+
+ <?php
+
+echo date("r");
+
+?>
+
+</body>
+
+</html>
